39491241f47af57ec32d9587994429132937409f
David Ramos <davidramos.13@outlook.com>
2023-11-28 22:07:15 +0100

    style prefixes

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -1,13 +1,23 @@
 
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generatedClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
 
 const App = () => (
-  <div>
-    <h1>Hi there!</h1>
-    <hr />
-    <MarketingApp />
-  </div>
+  <BrowserRouter>
+    <StylesProvider generateClassName={generatedClassName}>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </StylesProvider>
+  </BrowserRouter>
 ) ;
 
 export default App;
